420f16e518d7a23bc731e762210ac14a6a4bfbbf
Digital <Digital@DESKTOP-Q40K6VU.gsp.dc>
2026-03-06 13:17:21 -0300

    Adição da classe User e ajustes nas classes herdadas da Pet

---
+++ src/main/java/api/ache_pet/modules/comuns/enuns/ESituation.java
@@ -0,0 +1,6 @@
+package api.ache_pet.modules.comuns.enuns;
+
+public enum ESituation {
+    ACTIVE,
+    INACTIVE
+}
---
+++ src/main/java/api/ache_pet/modules/user/model/User.java
@@ -0,0 +1,48 @@
+package api.ache_pet.modules.user.model;
+
+
+import api.ache_pet.modules.comuns.enuns.ESituation;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table(name = "USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "NAME", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "PHONE", nullable = false, length = 11)
+    private String phone;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "DATE_REGISTER", nullable = false)
+    private LocalDateTime  dateRegister;
+
+    @Column(name = "SITUATION")
+    private ESituation situation;
+
+    @PrePersist
+    public void prePersist(){
+        this.dateRegister = LocalDateTime.now();
+    }
+
+    public User(){
+        this.situation = ESituation.ACTIVE;
+    }
+
+}
